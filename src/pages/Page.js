dda1ec70d9ee388025a12b81a9f077cc5f40e418
Aleksander Kruk <kruk.aleksander@hotmail.com>
2021-08-30 17:42:25 +0200

    page basic styles part1

--- src/pages/Page.js
+++ src/pages/Page.js
@@ -6,10 +6,20 @@ export default function Page() {
   const staticUrl = `http://strapi.bezdomniaki.com${window.location.pathname}`;
   useFetchArticle(staticUrl);
   if (article) {
-    const { tytul, opis } = article;
+    console.log(article);
+    const { tytul, opis, published_at, zdjecie } = article;
+
     return (
       <main className="page">
         <h1 className="page__title">{tytul}</h1>
+        {zdjecie ? (
+          <div className="page__image-wrapper">
+            <img
+              src={`http://strapi.bezdomniaki.com/${zdjecie.formats.medium.url}`}
+              className="page__main-image"
+            ></img>
+          </div>
+        ) : null}
         <div className="page__content">{opis}</div>
       </main>
     );
